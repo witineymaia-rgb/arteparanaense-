da69f6d95d392439923026fcac8999879539d0c6
witineymaia-rgb <witiney.maia@escola.pr.gov.br>
2026-09-09 17:29:06 -0300

    atualisei o meu codigo

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const botao = document. queryselector("#botaoMensagem");
+const mensagem =document.queryselector("#mensagemIntera
+    botao.addEventListener("click", mostrarmensagem);
+    function mostrarmensagem(){
+        mensagem.textcontent =
+        "voce acabou de criar uma interacao com javascri"
+
 
