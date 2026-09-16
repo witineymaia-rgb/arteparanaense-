ae2b72ace1a5ecd3a94342f422820887384f18b8
witineymaia-rgb <witiney.maia@escola.pr.gov.br>
2026-09-16 17:55:17 -0300

    atualisei o meu codigo

---
+++ script.js
@@ -0,0 +1,19 @@
+const botoesCurtir = document. querySelectorAll(. curtir);
+botoesCurtir.forEach(function(botaoCurtir){
+    let currti = false;
+    botoesCurtir. addEvenlistener("click , curtir");
+    function  curtir(){
+        const contador = botaoCurtir . querySelector("span");
+        if(curtiu === false){
+            contador . textContent++;
+            curtiu = true;{
+                else{
+                    contador .textContent--;
+                    curtiu = false;
+
+     }
+     }
+    });
+        
+        
+    
